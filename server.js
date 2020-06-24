ce7d4f6c825c8fc267211b48fb0fd50062f237b4
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-06-24 14:32:14 -0500

    Set up a user model and validation
    Validation done using express-validator

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 app.get("/", (req, res) => res.json({ msg: "Welcome to time tracker api" }));
 
 //Connect Database
-//connectDB();
+connectDB();
 
 //Init middleware
-//app.use(express.json({ entended: false }));
+app.use(express.json({ entended: false }));
 
 //define routes
 app.use("/api/users", require("./routes/users")); //import the file that pertains to that route
@@ -19,4 +19,4 @@ app.use("/api/timesheet", require("./routes/timesheet"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => `Server started on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
