73c2d61ac9310658625ae2960b499e2602694c1e
s0044613 <sama.mushtaq@rgare.com>
2020-06-25 11:51:20 -0500

    create and respond with JWT

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { body, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -17,14 +20,56 @@ router.post(
       "Please enter a password with 6 or more characters."
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      //check to see if a user already exists with that email
+      let user = await User.findOne({ email });
+      //send out response for bad request
+      if (user) {
+        return res.status(400).json({ msg: "User already exists." });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      //encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //  res.send("saved");
+
+      //create payload for JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).send("Server Error!");
+    }
   }
 );
 
