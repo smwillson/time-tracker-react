b2c26ba13b57fff36a2d41353698dd7566f84a8d
s0044613 <sama.mushtaq@rgare.com>
2020-07-10 17:13:03 -0500

    Create TemperatureUnitForm.js

---
+++ client/src/components/temperature/TemperatureUnitForm.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+
+import { TempUnitContext } from "../../context/temperature/TemperatureContext";
+
+const TemperatureUnitForm = () => {
+  const { tempUnit, setTempUnit } = useContext(TempUnitContext);
+  const { type } = tempUnit;
+  const onChange = (event) => {
+    setTempUnit({
+      type: event.target.value,
+    });
+    //console.log(tempUnit);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <span>Units:</span>
+      <p>
+        <label>
+          <input
+            className='with-gap'
+            name='type'
+            type='radio'
+            value='Celsius'
+            checked={type === "Celsius"}
+            onChange={onChange}
+          />
+          <span>Celsius</span>
+        </label>
+      </p>
+      <p>
+        <label>
+          <input
+            className='with-gap'
+            name='type'
+            type='radio'
+            value='Fahrenheit'
+            checked={type === "Fahrenheit"}
+            onChange={onChange}
+          />
+          <span>Fahrenheit</span>
+        </label>
+      </p>
+    </form>
+  );
+};
+
+export default TemperatureUnitForm;
