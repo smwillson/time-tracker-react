640785c53ab91bfd1414988dab6903fdf9aca7a7
s0044613 <sama.mushtaq@rgare.com>
2020-07-10 17:15:52 -0500

    Update temperatureContext.js

--- client/src/context/temperature/temperatureContext.js
+++ client/src/context/temperature/temperatureContext.js
@@ -2,15 +2,21 @@ import React, { useState, createContext } from "react";
 
 export const TemperatureContext = createContext();
 export const ForecastContext = createContext();
+export const TempUnitContext = createContext();
 
 const TemperatureContextProvider = (props) => {
   const [temperature, setTemperature] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [tempUnit, setTempUnit] = useState({
+    type: "Celsius",
+  });
 
   return (
     <TemperatureContext.Provider value={{ temperature, setTemperature }}>
       <ForecastContext.Provider value={{ forecast, setForecast }}>
-        {props.children}
+        <TempUnitContext.Provider value={{ tempUnit, setTempUnit }}>
+          {props.children}
+        </TempUnitContext.Provider>
       </ForecastContext.Provider>
     </TemperatureContext.Provider>
   );
