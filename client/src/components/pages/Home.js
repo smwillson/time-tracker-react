957d543447fc85172c725923a6b35c8a57145983
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-09-14 14:38:27 -0500

    Update Home.js
    Add timesheet list to home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import TimesheetList from "../timesheets/TimesheetList";
+import TimesheeList from "../timesheets/TimesheetList";
 
 const Home = () => {
   return (
-    <div>
-      <h3>Home page</h3>
+    <div className='container'>
+      <div className='row'>
+        <div col s6>
+          {/*TODO: Timesheet Form */}
+        </div>
+        <div col s6>
+          <TimesheetList />
+        </div>
+      </div>
     </div>
   );
 };
